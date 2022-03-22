175e81640753de344998eecc000ec861c993b58d
manLevPeng <rhdpfud@naver.com>
2022-03-22 20:53:56 +0900

    reC++_1

---
+++ prac/Project4_c++/01 cal.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+using namespace std;
+class Calculator
+{
+public:
+	void Init();
+	void ShowOpCount();
+	float Add(float a, float b);	//덧셈
+	float Min(float a, float b);	//뺄셈
+	float Mul(float a, float b);	//곱셈
+	float Div(float a, float b);	//나눗셈
+	
+private:
+	int AddCount;
+	int MinCount;
+	int MulCount;
+	int DivCount;
+};
+void Calculator::Init()
+{
+	AddCount = MinCount = MulCount = DivCount = 0;
+}
+void Calculator::ShowOpCount()
+{
+	cout << endl<<"덧셈 : " << AddCount << endl << "뺄셈 : " << MinCount << endl << "곱셈 : " << MulCount << endl << "나눗셈 : " << DivCount << endl;
+}
+float Calculator::Add(float a, float b)
+{
+	AddCount++;
+	return a + b;
+}
+float Calculator::Min(float a, float b)
+{
+	MinCount++;
+	return a - b;
+}
+float Calculator::Mul(float a, float b)
+{
+	MulCount++;
+	return a * b;
+}
+float Calculator::Div(float a, float b)
+{
+	DivCount++;
+	return a / b;
+}
+
+float main(void)
+{
+	Calculator cal;
+	cal.Init();
+	cout << "3.2 + 2.4 = " << cal.Add(3.2, 2.4) << endl;
+	cout << "2.2 - 1.5 = " << cal.Min(2.2, 1.5) << endl;
+	cout << "3.0 x 2.5 = " << cal.Mul(3.0, 2.5) << endl;
+	cout << "3.5 / 1.7 = " << cal.Div(3.5, 1.7) << endl;
+	cout << "4.9 / 1.2 = " << cal.Div(4.9, 1.2) << endl;
+	cal.ShowOpCount();
+	return 0;
+}
---
+++ prac/Project4_c++/03.cpp
@@ -0,0 +1,60 @@
+﻿#include<iostream>
+using namespace std;
+class Point
+{
+public:
+	void Init(int x, int y)
+	{
+		xpos = x;	ypos = y;
+	}
+	void ShowPointInfo() const
+	{
+		cout << "[" << xpos << ", " << ypos << "]" << endl;
+	}
+private:
+	int xpos, ypos;
+};
+class Circle			//Point 클래스 기반으로 원을 의미하는 클래스
+{
+public:
+	void Init(int x, int y, int r)
+	{
+		rad = r;
+		center.Init(x, y);
+	}
+	void ShowRingInfo()
+	{
+		cout << "radius : " << rad << endl;
+		center.ShowPointInfo();
+	}
+private:
+	int rad;			//반지름
+	Point center;		//원의 중심
+};
+class Ring				//Circle 클래스 기반으로 두 개의 원을 표현
+{
+public:
+	void Init(int inX, int inY, int inR, int outX, int outY, int outR)
+	{
+		inCircle.Init(inX, inY, inR);
+		outCircle.Init(outX, outY, outR);
+	}
+	void ShowRingInfo()
+	{
+		cout << "Inner Circle Info..." << endl;
+		inCircle.ShowRingInfo();
+		cout << "Outter Circle Info..." << endl;
+		outCircle.ShowRingInfo();
+	}
+private:
+	Circle inCircle;
+	Circle outCircle;
+};
+
+int main(void)
+{
+	Ring ring;
+	ring.Init(1, 1, 4, 2, 2, 9);
+	ring.ShowRingInfo();
+	return 0;
+}
