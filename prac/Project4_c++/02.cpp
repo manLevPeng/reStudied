99a46e58747a758b69534af6ea06ef48b34a8709
manLevPeng <rhdpfud@naver.com>
2022-03-22 21:04:15 +0900

    pr4_reC++_1

---
+++ prac/Project4_c++/02.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<cstring>
+using namespace std;
+class Printer
+{
+public:
+	void SetString(char* s);
+	void Showstring();
+
+private:
+	char mystring[30];
+};
+
+void Printer::SetString(char* s)
+{
+	strcpy(mystring, s);
+}
+
+void Printer::Showstring()
+{
+	cout << mystring << endl;
+}
+int main(void)
+{
+	Printer pnt;
+	pnt.SetString("Hello world!");
+	pnt.Showstring();
+
+	pnt.SetString("I love C++");
+	pnt.Showstring();
+
+	return 0;
+}
