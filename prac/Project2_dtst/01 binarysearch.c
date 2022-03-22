1a8980a998c72cb71e209635873fd2f4cc3d240c
manLevPeng <rhdpfud@naver.com>
2022-03-22 20:41:41 +0900

    Data Structure practice

---
+++ prac/Project2_dtst/01 binarysearch.c
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+int BSearch(int ar[], int len, int t)
+{
+	int head = 0, tail = len - 1, mid;
+	while (head <= tail) {
+		mid = (head + tail) / 2;
+		if (t == ar[mid])	return mid;
+		else {
+			if (t < ar[mid])		tail = mid - 1;
+			else /*t > ar[mid]*/	head = mid + 1;
+		}
+	}
+	return -1;
+}
+int ReBSearch(int ar[], int first, int last, int t)
+{
+	if (first > last)	return -1;
+	
+	int mid = (first + last) / 2;
+	if (ar[mid] == t)		return mid;
+	else if (ar[mid] < t)	return ReBSearch(ar, mid + 1, last, t);
+	else  /* ar[mid] > t */	return ReBSearch(ar, first, mid-1 , t);
+}
+void prtSR(int idx)
+{
+	if (idx == -1)	printf("타겟 탐색 실패\n");
+	else			printf("타겟 저장 인덱스 : %d\n", idx);
+}
+int main(void)
+{
+	int ar[] = { 1,3,5,7,9 }, idx;
+	idx = BSearch(ar, sizeof(ar) / sizeof(int), 7);
+	prtSR(idx);
+
+	idx = BSearch(ar, sizeof(ar) / sizeof(int), 4);
+	prtSR(idx);
+
+	idx = ReBSearch(ar, 0, sizeof(ar) / sizeof(int)-1, 7);
+	prtSR(idx);
+
+	idx = ReBSearch(ar, 0, sizeof(ar) / sizeof(int)-1, 4);
+	prtSR(idx);
+
+	return 0;
+}
---
+++ prac/Project2_dtst/02.1 Listmain.c
@@ -0,0 +1,50 @@
+﻿#include<stdio.h>
+#include "ArrayList.h"
+int main(void)
+{
+	//1번 리스트 생성 및 초기화
+	List list;
+	int data;
+	ListInit(&list);
+
+	//1번 1부터 9까지 리스트에 저장
+	for (int i = 1; i <= 9; i++)
+		LInsert(&list, i);
+
+	printf("현재 데이터의 수 : %d\n", LCount(&list));
+	
+	//2번 리스트에 저장된 값 순차 참조하여 값 합산, 출력
+	int sum=0;
+
+	if (LFirst(&list, &data)) {			//첫 번째 데이터 조회
+		sum += data;
+		printf("%d\n", sum);
+
+		while (LNext(&list, &data)) {		//두 번째 이후의 데이터 조회
+			sum += data;
+			printf("%d\n", sum);
+		}
+	}
+	printf("\n\n");
+
+	//3번 2의 배수와 3의 배수 탐색하여 모두 삭제
+
+	if (LFirst(&list, &data)) {
+		if (data%2==0 || data % 3 == 0)	LRemove(&list);
+
+		while (LNext(&list, &data))
+			if (data%2==0 || data % 3 == 0)	LRemove(&list);
+	}
+	//남은 데이터 전체 출력
+	printf("현재 데이터의 수 : %d\n", LCount(&list));
+
+	if (LFirst(&list, &data)) {			
+		printf("%d ", data);
+
+		while(LNext(&list, &data))		
+			printf("%d ", data);
+	}
+	printf("\n\n");
+
+	return 0;
+}
